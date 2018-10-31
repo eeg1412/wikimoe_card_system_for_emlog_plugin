d579d0cb10337b5fcdb6160ff0284d4ad4b11a3d
eeg1412 <eeg1412@163.com>
2018-10-31 09:44:04 +0800

    增加了万圣节卡背

--- wm_card.php
+++ wm_card.php
@@ -12,7 +12,7 @@ function wm_card_loghook() {
 	$wm_card_jsfile = BLOG_URL.'content/plugins/wm_card/card/card.js?ver=0.61';
 	$wm_card_layerjsfile = BLOG_URL.'content/plugins/wm_card/layer/layer.js';
 	$wm_card_layercssfile = BLOG_URL.'content/plugins/wm_card/layer/theme/default/layer.css';
-	$wm_card_cssfile = BLOG_URL.'content/plugins/wm_card/card/card.css?ver=0.6';
+	$wm_card_cssfile = BLOG_URL.'content/plugins/wm_card/card/card.css?ver=0.61';
 	$wm_card_pluginpath = BLOG_URL.'content/plugins/wm_card/';
 	$wmCard_set=unserialize(ltrim(file_get_contents(dirname(__FILE__).'/wm_card.com.php'),'<?php die; ?>'));
 	$wm_card_img_path = empty($wmCard_set['cdn'])?$wm_card_pluginpath.'card/img/':$wmCard_set['cdn'];
